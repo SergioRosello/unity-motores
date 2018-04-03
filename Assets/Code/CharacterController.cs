ed4903acd8000a945ee2a1bb5d4dbfe6ee809b26
SergioRosello <sergio-rosello@hotmail.com>
2018-04-03 20:19:02 +0200

    Added basic movement

---
+++ Assets/Code/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MovementController {
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+		velocity = new Vector2(5 * horizontal, 5 * vertical);
+	}
+
+	protected override void DetermineDirection(){
+		horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw("Vertical");
+
+	}
+}
---
+++ Assets/Code/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementController : MonoBehaviour {
+
+	protected float horizontal;
+	protected float vertical;
+	protected Rigidbody2D rb;
+	protected Vector2 velocity;
+	
+	protected abstract void DetermineDirection();
+	
+	// Use this for initialization
+	virtual protected void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	virtual protected void Update () {
+		DetermineDirection();
+	}
+
+	virtual protected void LateUpdate(){
+		rb.velocity = velocity;
+	}
+}
