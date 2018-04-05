e556f288e9137fed96e202849e7020f01e8e6611
SergioRosello <sergio-rosello@hotmail.com>
2018-04-05 11:59:12 +0200

    Setting up game code

---
+++ Assets/Code/BasicEnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyController : MovementController {
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+		// rb.position = Camera.main.ViewportToWorldPoint(new Vector2(Random.Range(0, Screen.width), Screen.height + 3));
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+		velocity = new Vector2(0, -maxSpeed);
+		Debug.Log("velocity: " + velocity);
+	}
+
+	protected override void DetermineDirection(){
+	}
+}
---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject basicEnemy;
+	public Rigidbody2D rigidbody;
+	void Start () {
+		basicEnemy = GetComponent<GameObject>();
+		PoolManager.Spawn(basicEnemy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
