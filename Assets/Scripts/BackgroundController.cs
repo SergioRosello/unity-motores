1eba1f0bd271d514d18c412c41ab2671d42bbeea
SergioRosello <sergio-rosello@hotmail.com>
2018-04-05 16:27:19 +0200

    Added background and it's speed

---
+++ Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	// Use this for initialization
+	private Transform transform;
+	private Vector3 velocity;
+	public float mapSpeed = 5f;
+	void Start () {
+		transform = GetComponent<Transform>();
+		velocity = new Vector3(0f, mapSpeed, 0f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position += -velocity * Time.fixedDeltaTime;
+	}
+}
