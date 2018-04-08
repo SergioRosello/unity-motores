7d1ddb4838ac786daaf4890b8c3d675175b0a5df
SergioRosello <sergio-rosello@hotmail.com>
2018-04-08 13:55:06 +0200

    Basic Enemy Spawner done

--- Assets/waveManager.cs
+++ Assets/waveManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class waveManager : MonoBehaviour {
 
+	public List<GameObject> Wave;
 	// Use this for initialization
 	void Start () {
-		
+		PoolManager.Load(Wave[0], Wave.Count);
+		foreach( GameObject enemy in Wave) {
+			PoolManager.Spawn(enemy);
+		}
 	}
 	
 	// Update is called once per frame
