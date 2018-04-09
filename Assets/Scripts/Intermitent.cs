6d07c7aad8121e2506578df3b09e749bbb96ac73
SergioRosello <sergio-rosello@hotmail.com>
2018-04-09 11:29:28 +0200

    Added intermitent script don't know why it doesn't work correctly

---
+++ Assets/Scripts/Intermitent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intermitent : MonoBehaviour {
+
+	public float timer;
+  void Start(){  
+    StartCoroutine("HideUnhide");
+	}
+
+
+  IEnumerator HideUnhide(){
+    while (true) {
+        yield return (new WaitForSeconds(timer));
+        gameObject.SetActive(true);
+        yield return (new WaitForSeconds(timer));
+        gameObject.SetActive(false);
+    }
+  }
+}
